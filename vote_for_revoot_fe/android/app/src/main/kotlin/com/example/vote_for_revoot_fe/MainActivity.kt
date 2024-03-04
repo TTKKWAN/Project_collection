97f25bb617905cd5f2ab3ef064b72565dbc67024
TTKKWAN <147288887+TTKKWAN@users.noreply.github.com>
2024-03-04 11:04:06 +0900

    Delete vote_for_revoot_fe directory

--- vote_for_revoot_fe/android/app/src/main/kotlin/com/example/vote_for_revoot_fe/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vote_for_revoot_fe
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- vote_for_revoot_fe/lib/text.dart
+++
@@ -1,1 +0,0 @@
-List<String> listcontent = ['T_K', 'hello i am Tae kwan, nice to meet to you'];
